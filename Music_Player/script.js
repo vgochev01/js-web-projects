092611829e025d014d711bc2ad8bc41024d1b45b
Valeri <csvalkata@gmail.com>
2021-04-12 17:19:12 +0300

    fixed some next/prev song problems

--- Music_Player/script.js
+++ Music_Player/script.js
@@ -21,11 +21,20 @@ const songs = [
         artist: "Jacinto 2",
         imgSrc: './img/jacinto-2.jpg',
         src: "./music/jacinto-2.mp3"
+    },
+    {
+        title: "Third Song",
+        artist: "Jacinto 3",
+        imgSrc: './img/jacinto-3.jpg',
+        src: "./music/jacinto-3.mp3"
     }
 ]
 
 let currentTime = 0;
 let currentSongIndex = 0;
+let songDuration = 0;
+
+switchSong();
 
 playPauseBtn.addEventListener("click", () => {
   if (audioElement.paused) {
@@ -44,23 +53,32 @@ nextBtn.addEventListener('click', () => {
     currentSongIndex = currentSongIndex == songs.length - 1 ? 0 : currentSongIndex + 1;
     switchSong();
 })
+
+audioElement.addEventListener('loadedmetadata', () => {
+    const songDuration = Math.floor(audioElement.duration);
+    const minutes = Math.floor(songDuration / 60);
+    const seconds = songDuration % 60;
+    duration.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+})
+
+audioElement.addEventListener('playing', () => {
+
+})
+
 function switchSong() {
     title.textContent = songs[currentSongIndex].title;
     artist.textContent = songs[currentSongIndex].artist;
     image.src = songs[currentSongIndex].imgSrc;
-    audioElement.src = null;
+    audioElement.src = songs[currentSongIndex].src;
     currentTime = 0;
     playPauseBtn.classList.contains('fa-pause') ? playPauseBtn.classList.replace('fa-pause', 'fa-play') : '';
 }
 
 function playSong(ev) {
-  audioElement.src = songs[currentSongIndex].src;
-  audioElement.onloadedmetadata = () => {
     audioElement.currentTime = currentTime;
     audioElement.play();
     playPauseBtn.setAttribute("title", "Pause");
     playPauseBtn.classList.replace("fa-play", "fa-pause");
-  };
 }
 
 function pauseSong() {
