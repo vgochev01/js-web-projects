29bd9b86bd94c1bb0d06f368dc2da3f89f956573
Valeri <csvalkata@gmail.com>
2021-04-18 16:45:05 +0300

    video player functionality to do

---
+++ Video_Player/script.js
@@ -0,0 +1,53 @@
+const player = document.querySelector('.player');
+const video = document.querySelector('.video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const speed = document.querySelector('.player-speed');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen');
+
+// Play & Pause ----------------------------------- //
+
+
+// On video end, show play button icon
+video.addEventListener('ended', showPlayIcon);
+
+// Progress Bar ---------------------------------- //
+
+// Format current time, duration
+
+// Update progress bar as video plays
+
+// Click to seek within the video
+
+// Volume Controls --------------------------- //
+
+// Mute
+
+// Volume Bar
+
+// Change Playback Speed -------------------- //
+
+// Fullscreen ------------------------------- //
+
+/* View in fullscreen */
+
+/* Close fullscreen */
+
+// Toggle fullscreen
+
+// Event Listeners
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
+volumeRange.addEventListener('click', changeVolume);
+volumeIcon.addEventListener('click', toggleMute);
+speed.addEventListener('change', changeSpeed);
+fullscreenBtn.addEventListener('click', toggleFullscreen);
